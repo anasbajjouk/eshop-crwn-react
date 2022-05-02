e6c2cc43bc3c73ff49896a2d43a3fa6a1098e0ea
Anas Bajjouk <bajjouk1@gmail.com>
2022-05-02 23:10:15 +0200

    changing button to styled comp

--- src/components/sign-in-form/sign-in-form.components.jsx
+++ src/components/sign-in-form/sign-in-form.components.jsx
@@ -3,9 +3,8 @@ import {
   signInAuthUserWithEmailAndPassword,
   signInWithGooglePopup,
 } from "../../utils/firebase/firebase.utils"
-import Button from "../button/button.component"
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component"
 import FormInput from "../form-input/form-input.component"
-
 import "./sign-in-form.styles.scss"
 
 const defaultFormFields = {
@@ -77,7 +76,10 @@ const SignInForm = () => {
 
         <div className="buttons-container">
           <Button type="submit">Sign In</Button>
-          <Button type="button" onClick={signInWithGoogle} buttonType="google">
+          <Button
+            type="button"
+            onClick={signInWithGoogle}
+            buttonType={BUTTON_TYPE_CLASSES.google}>
             Google Sign In
           </Button>
         </div>
