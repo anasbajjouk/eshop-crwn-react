3a7c123cfd85da3f949882775c233301e1b9160b
Anas Bajjouk <bajjouk1@gmail.com>
2022-05-22 17:05:12 +0200

    firebase fixed issue

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -105,7 +105,7 @@ export const createUserDocumentFromAuth = async (
     }
   }
 
-  return userDocRef
+  return userSnapshot
 }
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
