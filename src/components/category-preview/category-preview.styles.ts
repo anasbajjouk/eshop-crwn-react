e26820382d7d8bfd2e0dc2f2c02ce39154a405e6
Anas Bajjouk <bajjouk1@gmail.com>
2022-06-17 17:59:14 +0200

    adding some mobile styling

--- src/components/category-preview/category-preview.styles.ts
+++ src/components/category-preview/category-preview.styles.ts
@@ -1,24 +1,31 @@
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
+import { Link } from "react-router-dom"
+import styled from "styled-components"
 
 export const CategoryPreviewContainer = styled.div`
   display: flex;
   flex-direction: column;
   margin-bottom: 30px;
 
+  @media screen and (max-width: 800px) {
+    align-items: center;
+  }
 `
 
 export const CategoryPreviewLinkTitle = styled(Link)`
-    font-size: 28px;
-    margin-bottom: 25px;
-    cursor: pointer;
+  font-size: 28px;
+  margin-bottom: 25px;
+  cursor: pointer;
 `
 
 export const Preview = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  column-gap: 20px;
+
+  @media screen and (max-width: 800px) {
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    column-gap: 20px;
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 15px;
+    grid-row-gap: 25px;
+  }
 `
-
-
-
--- src/routes/navigation/navigation.styles.ts
+++ src/routes/navigation/navigation.styles.ts
@@ -7,12 +7,23 @@ export const NavigationContainer = styled.div`
   justify-content: space-between;
   margin-bottom: 25px;
   align-items: center;
+
+  @media screen and (max-width: 800px) {
+    height: 60px;
+    padding: 10px 20px;
+    margin-bottom: 20px;
+  }
 `
 
 export const LogoContainer = styled(Link)`
   height: 100%;
   width: 70px;
   padding: 25px;
+
+  @media screen and (max-width: 800px) {
+    width: 50px;
+    padding: 0;
+  }
 `
 
 export const NavLinksContainer = styled.div`
@@ -23,6 +34,10 @@ export const NavLinksContainer = styled.div`
   justify-content: flex-end;
   & > a {
     margin-right: 10px;
+  }
+
+  @media screen and (max-width: 800px) {
+    width: 80%;
   }
 `
 
